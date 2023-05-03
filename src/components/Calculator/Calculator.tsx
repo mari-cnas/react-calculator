95f4d8e65a006b94d233a32c17e36a0150b4646d
Mari CN <contatemariana@gmail.com>
2023-05-02 22:11:08 -0300

    feat:operations

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -10,37 +10,70 @@ interface IBaseComponentProps {
 }
 
 const Calculator: React.FC<IBaseComponentProps> = ({ children }) => {
-  const characters = [
-    'AC',
-    '+/-',
-    '%',
-    '/',
-    7,
-    8,
-    9,
-    'x',
-    4,
-    5,
-    6,
-    '-',
-    1,
-    2,
-    3,
-    '+',
-    0,
-    ',',
-    '*',
-    '=',
-  ]
   const [num, setNum] = useState(0)
+  const [oldNum, setOldNum] = useState(0)
+  const [operator, setOperator] = useState()
 
-  const handleInput = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const input = e.target.value
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const input = event.target.value
+    if (num === 0) {
+      setNum(input)
+    } else {
       setNum(num + input)
-    },
-    [num],
-  )
+    }
+  }
+
+  const handleClear = (): [
+    number,
+    React.Dispatch<React.SetStateAction<number>>,
+  ] => {
+    setNum(0)
+    return [num, setNum]
+  }
+
+  const porcentage = (): [
+    number,
+    React.Dispatch<React.SetStateAction<number>>,
+  ] => {
+    setNum(num / 100)
+    return [setNum]
+  }
+
+  const changeSign = (): [
+    number,
+    React.Dispatch<React.SetStateAction<number>>,
+  ] => {
+    if (num > 0) {
+      setNum(-num)
+    } else {
+      setNum(Math.abs)
+    }
+    return [setNum]
+  }
+
+  const operatorHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    const operatorInput = event.target.value
+    setOperator(operatorInput)
+    setOldNum(num)
+    setNum(0)
+  }
+
+  const calculate = (): [
+    number,
+    React.Dispatch<React.SetStateAction<number>>,
+  ] => {
+    if (operator === '/') {
+      setNum(Number(oldNum) / Number(num))
+    } else if (operator === 'x') {
+      setNum(Number(oldNum) * Number(num))
+    } else if (operator === '+') {
+      setNum(Number(oldNum) + Number(num))
+    } else if (operator === '-') {
+      setNum(Number(oldNum) - Number(num))
+    }
+  }
 
   return (
     <>
@@ -50,16 +83,66 @@ const Calculator: React.FC<IBaseComponentProps> = ({ children }) => {
       <Wrapper maxWidth="xs">
         <h1 className="result">{num}</h1>
         <Box>
-          {characters.map((character) => (
-            <Btn
-              type="button"
-              key={character}
-              value={character}
-              onClick={handleInput}
-            >
-              {character}
-            </Btn>
-          ))}
+          <Btn type="button" value={7} onClick={handleClear}>
+            AC
+          </Btn>
+          <Btn type="button" value={7} onClick={changeSign}>
+            +/-
+          </Btn>
+          <Btn type="button" value={7} onClick={porcentage}>
+            %
+          </Btn>
+          <Btn type="button" value="/" onClick={operatorHandler}>
+            /
+          </Btn>
+          <Btn type="button" value={7} onClick={handleInput}>
+            7
+          </Btn>
+          <Btn type="button" value={8} onClick={handleInput}>
+            8
+          </Btn>
+          <Btn type="button" value={9} onClick={handleInput}>
+            9
+          </Btn>
+          <Btn type="button" value="x" onClick={operatorHandler}>
+            x
+          </Btn>
+          <Btn type="button" value={4} onClick={handleInput}>
+            4
+          </Btn>
+          <Btn type="button" value={5} onClick={handleInput}>
+            5
+          </Btn>
+          <Btn type="button" value={6} onClick={handleInput}>
+            6
+          </Btn>
+          <Btn type="button" value="-" onClick={operatorHandler}>
+            -
+          </Btn>
+          <Btn type="button" value={1} onClick={handleInput}>
+            1
+          </Btn>
+          <Btn type="button" value={2} onClick={handleInput}>
+            2
+          </Btn>
+          <Btn type="button" value={3} onClick={handleInput}>
+            3
+          </Btn>
+          <Btn type="button" value="+" onClick={operatorHandler}>
+            +
+          </Btn>
+          <Btn type="button" value={0} onClick={handleInput}>
+            0
+          </Btn>
+          <Btn type="button" value="." onClick={handleInput}>
+            ,
+          </Btn>
+          <Btn type="button" value={0} onClick={operatorHandler}>
+            *
+          </Btn>
+          <Btn type="button" value="=" onClick={calculate}>
+            =
+          </Btn>
         </Box>
       </Wrapper>
     </>
